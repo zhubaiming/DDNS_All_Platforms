f4f1a3f74aece218743ed102749dee6c7f1e2fab
baiming.zhu <zbm4333@gmail.com>
2022-09-20 14:53:13 +0800

    添加自定义异常

---
+++ Application/Exceptions/CliException.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+class CliException extends Exception
+{
+    public function __construct(string $message, int $code = 0, ?Throwable $previous = null)
+    {
+        if ('local' !== env('app.env')) {
+            set_exception_handler(function ($previous) {
+                $text = '[' . $_SERVER['REQUEST_TIME'] . '] - [error] - [' . $previous->getCode() . '] - ' . $previous->getMessage() . PHP_EOL . PHP_EOL;
+                if (env('app.debug')) echo $text;
+                log($text);
+            });
+        }
+
+        parent::__construct($message, $code, $previous);
+    }
+}
